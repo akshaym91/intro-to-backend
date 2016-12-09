3199c2db63197d055aa7626bd1be76afa556cf75
Akshay Menon <akshaym91@gmail.com>
2016-12-09 15:26:00 +0530

    Added the user signup page to fit all requirements of the the assignments

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-## Python Flask Skeleton for Google App Engine
+## Python Backend course on Udacity
 
-![status: inactive](https://img.shields.io/badge/status-inactive-red.svg)
+###Assignment and Tasks
 
-
-A skeleton for building Python applications on Google App Engine with the
-[Flask micro framework](http://flask.pocoo.org).
-
-See our other [Google Cloud Platform github
-repos](https://github.com/GoogleCloudPlatform) for sample applications and
-scaffolding for other python frameworks and use cases.
+A complete directory of the Tasks and assignments for the [Intro to Backend](https://www.udacity.com/course/intro-to-backend--ud171) course on Udacity.
+Taken as part of the [Fullstack Web Development](https://www.udacity.com/degrees/full-stack-web-developer-nanodegree--nd004).
 
 ## Run Locally
 1. Install the [App Engine Python SDK](https://developers.google.com/appengine/downloads).
@@ -17,13 +12,13 @@ See the README file for directions. You'll need python 2.7 and [pip 1.4 or later
 2. Clone this repo with
 
    ```
-   git clone https://github.com/GoogleCloudPlatform/appengine-python-flask-skeleton.git
+   https://github.com/akshaym91/intro-to-backend.git
    ```
 3. Install dependencies in the project's lib directory.
    Note: App Engine can only import libraries from inside your project directory.
 
    ```
-   cd appengine-python-flask-skeleton
+   cd intor-to-backend
    pip install -r requirements.txt -t lib
    ```
 4. Run this project locally from the command line:
@@ -40,19 +35,12 @@ for options when running dev_appserver.
 ## Deploy
 To deploy the application:
 
-1. Use the [Admin Console](https://appengine.google.com) to create a
-   project/app id. (App id and project id are identical)
-1. [Deploy the
-   application](https://developers.google.com/appengine/docs/python/tools/uploadinganapp) with
-
-   ```
-   appcfg.py -A <your-project-id> --oauth2 update .
-   ```
-1. Congratulations!  Your application is now live at your-app-id.appspot.com
-
-## Next Steps
-This skeleton includes `TODO` markers to help you find basic areas you will want
-to customize.
+How to use the google cloud platform:
+```
+$ dev_appserver.py $PWD
+```
+$ gcloud app deploy app.yaml --project <project-name>
+```
 
 ### Relational Databases and Datastore
 To add persistence to your models, use
@@ -68,10 +56,6 @@ page for libraries that are already included in the SDK.  To include SDK
 libraries, add them in your app.yaml file. Other than libraries included in
 the SDK, only pure python libraries may be added to an App Engine project.
 
-### Feedback
-Star this repo if you found it useful. Use the github issue tracker to give
-feedback on this repo.
-
 ## Contributing changes
 See [CONTRIB.md](CONTRIB.md)
 
@@ -79,4 +63,4 @@ See [CONTRIB.md](CONTRIB.md)
 See [LICENSE](LICENSE)
 
 ## Author
-Logan Henriquez and Johan Euphrosine
+Akshay Menon
--- template.py
+++ template.py
@@ -2,6 +2,7 @@ import os
 import webapp2
 import jinja2
 import codecs
+import re
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
@@ -26,8 +27,7 @@ class MainPage(Handler):
     """docstring for MainPage"""
 
     def get(self):
-        items = self.request.get_all('food')
-        self.render("shopping_list.html", items=items)
+        self.render("backend.html")
 
 
 class FizzBuzz(Handler):
@@ -37,14 +37,84 @@ class FizzBuzz(Handler):
         self.render("fizzbuzz.html")
 
 
+class TodoList(Handler):
+    """docstring for TodoList"""
+
+    def get(self):
+        items = self.request.get_all('food')
+        self.render("shopping_list.html", items=items)
+
+
+class SignUp(Handler):
+    """docstring for SignUp"""
+
+    def get(self):
+        errors = {"username_error": "",
+                  "password_error": "",
+                  "verify_error": "",
+                  "email_error": "",
+                  "user_username": "",
+                  "user_email": ""}
+        self.render("signup.html", errors=errors)
+
+    def post(self):
+
+        def validate_username(username):
+            USERNAME_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+            return USERNAME_RE.match(username)
+
+        def validate_email(email):
+            EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+            if(EMAIL_RE.match(email) or email == ""):
+                return True
+            return EMAIL_RE.match(email)
+
+        def validate_password(password):
+            PASSWORD_RE = re.compile(r"^.{3,20}$")
+            return PASSWORD_RE.match(password)
+
+        user_username = self.request.get('username')
+        user_password = self.request.get('password')
+        user_verifypassword = self.request.get('verify')
+        user_email = self.request.get('email')
+
+        test_username = validate_username(user_username)
+        test_password = (validate_password(user_password) and
+                         (user_password == user_verifypassword))
+        test_email = validate_email(user_email)
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+        email_error = ""
+        if (not test_username):
+            username_error = "Invalid Username."
+
+        if (not validate_password(user_password)):
+            password_error = "Invalid Password."
+
+        if (not (user_password == user_verifypassword)):
+            verify_error = "Password mismatch."
+
+        if (not test_email):
+            email_error = "Invalid Email."
+
+        if not (test_username and
+                test_password and
+                test_email and
+                (user_password == user_verifypassword)):
+            self.render("signup.html",
+                        errors={"username_error": username_error,
+                                "password_error": password_error,
+                                "verify_error": verify_error,
+                                "email_error": email_error,
+                                "user_username": user_username,
+                                "user_email": user_email})
+        else:
+            self.render("thanks.html", user_username=user_username)
+
+
 class Rot13(Handler):
     """docstring for Rot13"""
-
-    def encode_to_rot13():
-        pass
-
-    def decode_from_rot13():
-        pass
 
     def get(self):
         self.render("rot13.html")
@@ -54,6 +124,16 @@ class Rot13(Handler):
         output_text = codecs.encode(input_text, 'rot_13')
         self.render("rot13.html", output_text=output_text)
 
+
+class ThanksHandler(Handler):
+    """docstring for ThanksHandler"""
+
+    def get(self):
+        self.render("thanks.html")
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/fizzbuzz', FizzBuzz),
-                               ('/rot13', Rot13)], debug=True)
+                               ('/todolist', TodoList),
+                               ('/signup', SignUp),
+                               ('/rot13', Rot13),
+                               ('/thanks', ThanksHandler)], debug=True)
